d7f58c5a674c9c68b786e74874bf8861e4e1d05e
NamNoise <nam.hoang916@education.nsw.gov.au>
2025-08-14 13:08:24 +0000

    Nam's Work

--- items.py
+++
@@ -1,9 +0,0 @@
-class Item:
-    def __init__(self, item_id, name, item_type="misc", effect=""):
-        self.item_id = item_id
-        self.name = name
-        self.item_type = item_type
-        self.effect = effect
-
-    def __repr__(self):
-        return f"<Item {self.item_id}:{self.name}>"
